c4c6e48b22ec143e57eb404e542c4953b3922f51
Erik W. T. Gonçalves <40431371+erikwtg@users.noreply.github.com>
2025-06-15 20:37:52 -0300

    Adicionado componente ao storybook, ajustado configurações do typescript.

---
+++ src/stories/TranscriptionListSkeletom.stories.svelte
@@ -0,0 +1,15 @@
+<script module>
+	import { defineMeta } from "@storybook/addon-svelte-csf";
+	import TranscriptionListSkeleton from "../components/TranscriptionListSkeleton.svelte";
+
+	const { Story } = defineMeta({
+		title: "Transcription Components/TranscriptionListSkeleton",
+		component: TranscriptionListSkeleton,
+		tags: ["autodocs"],
+		argTypes: {
+			count: { control: "number" },
+		},
+	});
+</script>
+
+<Story name="TranscriptionListSkeleton" args={{ count: 1 }}/>
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,34 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+// import path from "path";
 
 export default defineConfig({
   plugins: [
     svelte(),
     tailwindcss(),
   ],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "Transcription",
-      fileName: (format) => `transcription.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["svelte"],
-      output: {
-        globals: {
-          svelte: "Svelte",
-        },
-      },
-    },
-    cssCodeSplit: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
-    },
-  },
+  // build: {
+  //   lib: {
+  //     entry: path.resolve(__dirname, "src/index.ts"),
+  //     name: "Transcription",
+  //     fileName: (format) => `transcription.${format}.js`,
+  //   },
+  //   rollupOptions: {
+  //     external: ["svelte"],
+  //     output: {
+  //       globals: {
+  //         svelte: "Svelte",
+  //       },
+  //     },
+  //   },
+  //   cssCodeSplit: true,
+  // },
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src/'),
+  //   },
+  // },
   server: {
     host: "0.0.0.0",
     port: 5173,
